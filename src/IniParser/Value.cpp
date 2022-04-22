b832257e3a1e58e431a67acf0326afc0d14af718
suicedek <handsomefox.hf@gmail.com>
2022-04-22 09:50:04 +0300

    Reformat, add << operator

--- src/IniParser/Value.cpp
+++ src/IniParser/Value.cpp
@@ -1,49 +1,73 @@
 #include "Value.h"
 #include "Utils.h"
 
-Value::Value(std::string value)
-        : _data(std::move(value)) {}
+Value::Value(std::string value) : _data(std::move(value)) {}
 
-bool Value::Empty() const {
-    return _data.empty();
+bool Value::Empty() const
+{
+  return _data.empty();
 }
 
-std::string Value::ToString() const {
-    return _data;
+std::string Value::ToString() const
+{
+  return _data;
 }
 
-int32_t Value::ToInt32() const {
-    if (_data.empty())
-        return 0;
+int32_t Value::ToInt32() const
+{
+  if (_data.empty())
+  {
+    return 0;
+  }
 
-    return std::stoi(_data);
+  return std::stoi(_data);
 }
 
-float Value::ToFloat() const {
-    if (_data.empty())
-        return 0.0f;
+float Value::ToFloat() const
+{
+  if (_data.empty())
+  {
+    return 0.0f;
+  }
 
-    return std::stof(_data);
+  return std::stof(_data);
 }
 
-double Value::ToDouble() const {
-    if (_data.empty())
-        return 0.0;
+double Value::ToDouble() const
+{
+  if (_data.empty())
+  {
+    return 0.0;
+  }
 
-    return std::stod(_data);
+  return std::stod(_data);
 }
 
-bool Value::ToBool() const {
-    bool is_alpha;
+bool Value::ToBool() const
+{
+  bool is_alpha = true;
 
-    if (contains(_data, '1') || contains(_data, '0'))
-        is_alpha = false;
-    else is_alpha = true;
+  if (contains(_data, '1') || contains(_data, '0'))
+  {
+    is_alpha = false;
+  }
 
-    bool ret;
+  bool ret;
 
-    if (is_alpha) std::istringstream(_data) >> std::boolalpha >> ret;
-    else std::istringstream(_data) >> ret;
+  if (is_alpha)
+  {
+    std::istringstream(_data) >> std::boolalpha >> ret;
+  }
+  else
+  {
+    std::istringstream(_data) >> ret;
+  }
 
-    return ret;
+  return ret;
+}
+
+std::ostream &operator<<(std::ostream &stream, const Value &val)
+{
+  stream << val._data;
+  return stream;
 }
--- src/IniParser/Value.h
+++ src/IniParser/Value.h
@@ -1,32 +1,38 @@
 #pragma once
 
-#include <string>
+#include <ostream>
 #include <sstream>
+#include <string>
 
-class Value {
+// Value represents parsed .ini key property value.
+// Contains conversions to different types.
+class Value
+{
 public:
-    explicit Value(std::string value);
+  explicit Value(std::string value);
 
-    Value() = default;
+  Value() = default;
 
-    // Return whether the value is empty
-    [[nodiscard]] bool Empty() const;
+  // Return whether the value is empty
+  [[nodiscard]] bool Empty() const;
 
-    // Return value as str.
-    [[nodiscard]] std::string ToString() const;
+  // Return value as str.
+  [[nodiscard]] std::string ToString() const;
 
-    // Return value as int32 using std::stoi or 0 if value is empty.
-    [[nodiscard]] int32_t ToInt32() const;;
+  // Return value as int32 using std::stoi or 0 if value is empty.
+  [[nodiscard]] int32_t ToInt32() const;
 
-    // Return value as float using std::stof or 0.0f if value is empty.
-    [[nodiscard]] float ToFloat() const;;
+  // Return value as float using std::stof or 0.0f if value is empty.
+  [[nodiscard]] float ToFloat() const;
 
-    // Return value as double using std::stod or 0.0 if value is empty.
-    [[nodiscard]] double ToDouble() const;;
+  // Return value as double using std::stod or 0.0 if value is empty.
+  [[nodiscard]] double ToDouble() const;
 
-    // Return value as bool (if string is either `0`/`1` or `true`/`false`.
-    [[nodiscard]] bool ToBool() const;;
+  // Return value as bool (if string is either `0`/`1` or `true`/`false`.
+  [[nodiscard]] bool ToBool() const;
+
+  friend std::ostream &operator<<(std::ostream &stream, const Value &val);
+
 private:
-
-    std::string _data;
+  std::string _data;
 };
